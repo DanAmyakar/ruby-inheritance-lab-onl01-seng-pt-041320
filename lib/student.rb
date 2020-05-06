cdea1f9f15a61a23960c838e26c4529d882483de
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-06 14:54:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,4 +2,16 @@ require_relative './user.rb'
 
 class Student < User
   
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(el)
+    @knowledge << el
+  end
+  
+  def knowledge
+    @knowledge
+  end
+  
 end
