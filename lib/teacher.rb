9fef4842adfd5da235d589829894fa6e756c20ab
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-06 15:01:45 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -5,11 +5,7 @@ class Teacher < User
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
   def teach
-    x != 0 ? x : x = 0
-    KNOWLEDGE[x]
-    x += 1
+    
   end
-  
-  binding.pry
   
 end
